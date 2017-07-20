cbf7562645f701b0b25d2e7f608b8cb34bd58f55
timtro <timtro@gmail.com>
2017-07-19 21:19:41 -0400

    Use Brown's detection idiom do detect nullary callable objects.

--- bench/function-currying.hpp
+++ bench/function-currying.hpp
@@ -1,40 +1,38 @@
+#include <experimental/type_traits>
 #include <functional>
+
+using std::experimental::is_detected_v;
 
 // No include guards. If you're including these outside of a benchmark, then
 // you're taking your life into your hands.
 
 namespace curry01 {
-  // Details: The simplest currying mechanism I could come up with. A SFINAE
-  // based trait detects a callable's need for arguments. The curry function,
-  // when called on a callable object, simply returns a variadic lambda object
-  // that will pack its argument(s) into a new variadic lambda object. This is
-  // packing and returning of unary lambdas is recursive, until the last
-  // required argument is passed. Then, the final lambda object is nullary, and
-  // simply invokes the nested lambdas.
+  // Details: The simplest currying mechanism I could come up with. A simple
+  // SFINAE trait using Walter Brown's detection idiom detects nullary
+  // callables. The curry function, when called on a non-nullary callable
+  // object, simply returns a variadic lambda object that will pack its given
+  // argument(s) into a new variadic lambda object. This is packing and
+  // returning of unary lambdas is recursive, until the last required argument
+  // is passed. Then, the final lambda object is nullary, and simply invokes the
+  // nested lambdas.
 
   namespace detail {
-    template <class, class = void>
-    struct needs_arguments : std::true_type {};
-
-    template <class T>
-    struct needs_arguments<
-        T, std::void_t<decltype(std::invoke(std::declval<T>()))>>
-        : std::false_type {};
+    template <typename T>
+    using is_nullary_t = decltype(std::invoke(std::declval<T>()));
 
     template <typename T>
-    constexpr bool needs_arguments_v = needs_arguments<T>::value;
+    constexpr bool is_nullary_v = is_detected_v<is_nullary_t, T>;
   } // namespace detail
 
   template <typename F>
   auto curry(F &&f) {
-    if constexpr (detail::needs_arguments_v<decltype(f)>)
+    if constexpr (detail::is_nullary_v<F>)
+      return std::invoke(f);
+    else
       return [=](auto &&x) {
         return curry([=](auto &&... xs) -> decltype(std::invoke(f, x, xs...)) {
           return std::invoke(f, x, xs...);
         });
       };
-    else
-      return std::invoke(f);
   }
-
 } // namespace curry01
