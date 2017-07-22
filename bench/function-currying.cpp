986ae5f9c907c2094f16ff87a118ea66fb64899d
timtro <timtro@gmail.com>
2017-07-22 04:27:57 -0400

    Abandoned https://goo.gl/gBvBmx, but adapted curryX for variadic funcs.

--- bench/function-currying.cpp
+++ bench/function-currying.cpp
@@ -2,9 +2,10 @@
 
 #include "function-currying.hpp"
 
-auto abcdefg = [](auto a, auto b, auto c, auto d, auto e, auto f, auto g) {
-  return a + b + c + d + e + f + g;
-};
+const auto abcdefg = [](auto a, auto b, auto c, auto d, auto e, auto f,
+                        auto g) { return a + b + c + d + e + f + g; };
+
+const auto devnull = [](auto... xs) -> decltype(auto) { return sizeof...(xs); };
 
 static void bm_byhand_calling(benchmark::State &state) {
   while (state.KeepRunning()) {
@@ -21,12 +22,20 @@ static void bm_curry01_calling(benchmark::State &state) {
 }
 BENCHMARK(bm_curry01_calling);
 
-static void bm_curry01_creation(benchmark::State &state) {
+static void bm_curry01X_calling(benchmark::State &state) {
   while (state.KeepRunning()) {
-    auto cur_callingbcdefg = curry01::curry(abcdefg);
+    curry01::curryX(devnull)("Call")("Me")("With")("any")("number")("of")(
+        "args")(curry01::call);
+  }
+}
+BENCHMARK(bm_curry01X_calling);
+
+static void bm_curry02_calling(benchmark::State &state) {
+  auto cur_callingbcdefg = curry02::curry(abcdefg);
+  while (state.KeepRunning()) {
     cur_callingbcdefg(1)(2)(3)(4)(5)(6)(7);
   }
 }
-BENCHMARK(bm_curry01_creation);
+BENCHMARK(bm_curry02_calling);
 
 BENCHMARK_MAIN();
--- bench/function-currying.hpp
+++ bench/function-currying.hpp
@@ -1,7 +1,11 @@
 #include <experimental/type_traits>
 #include <functional>
+#include <type_traits>
+#include <utility>
 
 using std::experimental::is_detected_v;
+
+// clang-format on
 
 // No include guards. If you're including these outside of a benchmark, then
 // you're taking your life into your hands.
@@ -16,6 +20,9 @@ namespace curry01 {
   // is passed. Then, the final lambda object is nullary, and simply invokes the
   // nested lambdas.
 
+  struct call_t {
+  } call;
+
   namespace detail {
     template <typename T>
     using is_nullary_t = decltype(std::invoke(std::declval<T>()));
@@ -25,14 +32,62 @@ namespace curry01 {
   } // namespace detail
 
   template <typename F>
-  auto curry(F &&f) {
+  auto curry(F f) {
     if constexpr (detail::is_nullary_v<F>)
       return std::invoke(f);
     else
-      return [=](auto &&x) {
-        return curry([=](auto &&... xs) -> decltype(std::invoke(f, x, xs...)) {
-          return std::invoke(f, x, xs...);
-        });
+      return [f](auto &&x) {
+        return curry(
+            // perfectly capture x here:
+            [f, x](auto &&... xs) -> decltype(std::invoke(f, x, xs...)) {
+              return std::invoke(f, x, xs...);
+            });
       };
   }
+
+  // This allows you to curry variadic functions. I got the idea of this from
+  // https://goo.gl/gBvBmx. This always returns a callable, but an argument of
+  // type call_t acts as a sentinal and forces the function call with previously
+  // bound arguments.
+  template <typename F>
+  auto curryX(F f) {
+    return [f](auto x) {
+      if constexpr (std::is_same<std::decay_t<decltype(x)>, call_t>::value)
+        return std::invoke(f);
+      else
+        return curryX(
+            // perfectly capture x here:
+            [f, x](auto &&... xs) -> decltype(std::invoke(f, x, xs...)) {
+              return std::invoke(f, x, xs...);
+            });
+    };
+  }
 } // namespace curry01
+
+namespace curry02 {
+  // The only difference between curry02 and curry01 is the use of std::invoke.
+  // Any difference in performance should be due to the universal calling
+  // overhead.
+  //
+  namespace detail {
+    template <typename T>
+    using is_nullary_t = decltype(std::invoke(std::declval<T>()));
+
+    template <typename T>
+    constexpr bool is_nullary_v = is_detected_v<is_nullary_t, T>;
+  } // namespace detail
+
+  template <typename F>
+  auto curry(F f) {
+    if constexpr (detail::is_nullary_v<F>)
+      return f();
+    else
+      return [f](auto &&x) {
+        return curry(
+            // perfectly capture x here:
+            [f, x](auto &&... xs) -> decltype(f(x, xs...)) {
+              return f(x, xs...);
+            });
+      };
+  }
+} // namespace curry02
