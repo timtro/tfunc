e9b02526015f165c4fbe80376936757ba6f20687
timtro <timtro@gmail.com>
2017-08-16 00:47:40 -0400

    Changed F<A> parameters to reference semantics

--- include/functor/foptional.hpp
+++ include/functor/foptional.hpp
@@ -1,21 +1,23 @@
 #pragma once
 
+#include <experimental/optional>
+
 #include "functor.hpp"
 
 #include "../function_traits.hpp"
 #include <algorithm>
-#include <experimental/optional>
 #include <experimental/type_traits>
 #include <functional>
 #include <type_traits>
 
+using std::experimental::make_optional;
 using std::experimental::optional;
 
 // fmap : (A → B) → F<A> → F<B>
 template <typename A, typename F>
-auto fmap(F f, const optional<A> oa) {
+auto fmap(F f, const optional<A> &oa) {
   if (oa) {
-    return make_optional(f(*oa));
+    return make_optional(std::invoke(f, *oa));
   } else
     return optional<trait::invoke_result_t<F, A>>{};
 }
