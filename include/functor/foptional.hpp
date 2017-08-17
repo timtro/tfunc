16ac89f42e97d034db51007fc4636d16d332d9b5
timtro <timtro@gmail.com>
2017-08-17 11:39:22 -0400

    Added rvalue ref overload to fmap until I implement perfect fwd.

--- include/functor/foptional.hpp
+++ include/functor/foptional.hpp
@@ -1,11 +1,9 @@
 #pragma once
 
-#include <experimental/optional>
-
-#include "functor.hpp"
-
 #include "../function_traits.hpp"
+#include "functor.hpp"
 #include <algorithm>
+#include <experimental/optional>
 #include <experimental/type_traits>
 #include <functional>
 #include <type_traits>
@@ -19,6 +17,17 @@ auto fmap(F f, const optional<A> &oa) {
   if (oa) {
     return make_optional(std::invoke(f, *oa));
   } else
+    // TODO: Replace with std::invoke_result_t when Clang implements it.
+    return optional<trait::invoke_result_t<F, A>>{};
+}
+
+// fmap : (A → B) → F<A> → F<B>
+template <typename A, typename F>
+auto fmap(F f, optional<A> &&oa) {
+  if (oa) {
+    return make_optional(std::invoke(f, std::move(*oa)));
+  } else
+    // TODO: Replace with std::invoke_result_t when Clang implements it.
     return optional<trait::invoke_result_t<F, A>>{};
 }
 
