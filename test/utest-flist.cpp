4eb140e88212567bc00e996a1d4d459780aa5c96
timtro <timtro@gmail.com>
2017-08-18 13:50:33 -0400

    Added constructor and assignment call tests for fmaps.

--- test/utest-flist.cpp
+++ test/utest-flist.cpp
@@ -5,9 +5,10 @@
 #include <catch/catch.hpp>
 
 #include "test-fixtures.hpp"
-using tst::A;  // Tag for unit type
-using tst::B;  // Tag for unit type
-using tst::C;  // Tag for unit type
+using tst::A; // Tag for unit type
+using tst::B; // Tag for unit type
+using tst::C; // Tag for unit type
+using tst::CtorLogger;
 using tst::D;  // Tag for unit type
 using tst::f;  // f : A → B
 using tst::g;  // g : B → C
@@ -30,7 +31,7 @@ TEST_CASE("The std::vector type constructor should be a functor:") {
   REQUIRE(tf::is_functor<std::vector>::value == true);
 }
 
-TEST_CASE("Given a std::vector<A> …") {
+TEST_CASE("Given a std::vector<A> …", "[fmap]") {
   std::vector<A> as{A{}};
 
   SECTION("… and a function f : A → B, fmap should produce a std::vector<B>:") {
@@ -62,11 +63,26 @@ TEST_CASE("Given a std::vector<A> …") {
   }
 }
 
+TEST_CASE("For std::vector, calling fmap should induce the same constructor "
+          "and assignment calls as simply using std::transform.",
+          "[CtorAs], [fmap]") {
+  std::vector<CtorLogger> a(2);
+  auto b = fmap(id, a);
+
+  std::vector<CtorLogger> ra(2);
+  auto rb = std::vector<CtorLogger>{};
+  rb.reserve(ra.size()); // If no reserve, you get extra copy ctrn!
+  std::transform(ra.cbegin(), ra.cend(), std::back_inserter(rb), id);
+
+  REQUIRE(b[0].flags == rb[0].flags);
+  REQUIRE(b[0].flags == b[1].flags);
+}
+
 TEST_CASE("The std::list typeconstructor should be a functor:") {
   REQUIRE(tf::is_functor<std::list>::value == true);
 }
 
-TEST_CASE("Given a std::list<A> …") {
+TEST_CASE("Given a std::list<A> …", "[fmap]") {
   std::list<A> as{A{}};
 
   SECTION("… and a function f : A → B, fmap should produce a std::list<B>:") {
@@ -99,11 +115,28 @@ TEST_CASE("Given a std::list<A> …") {
   }
 }
 
-TEST_CASE("The std::array typeconstructor should be a functor:") {
-  REQUIRE(tf::is_functor<std::list>::value == true);
+TEST_CASE("For std::list, calling fmap should induce the same constructor and "
+          "assignment calls as simply using std::transform.",
+          "[CtorAs], [fmap]") {
+  std::list<CtorLogger> a(2);
+  auto b = fmap(id, a);
+
+  std::list<CtorLogger> ra(2);
+  auto rb = std::list<CtorLogger>{};
+  std::transform(ra.cbegin(), ra.cend(), std::back_inserter(rb), id);
+
+  REQUIRE(b.begin()->flags == rb.begin()->flags);
+  REQUIRE(b.begin()->flags == (b.begin()++)->flags);
 }
 
-TEST_CASE("Given a std::array<A,·> …") {
+TEST_CASE("The std::array typeconstructor will not technically be a functor, "
+          "because it can not be constructed with the form F<A>",
+          "[FIXME]") {
+  REQUIRE("FIXME" == "FIXME");
+  // REQUIRE(tf::is_functor<std::array>::value == true);
+}
+
+TEST_CASE("Given a std::array<A,·> …", "[fmap]") {
   std::array<A, 1> as{{A{}}};
 
   SECTION("… and a function f : A → B, fmap should produce a std::array<B>:") {
@@ -134,11 +167,30 @@ TEST_CASE("Given a std::array<A,·> …") {
   }
 }
 
-TEST_CASE("The std::deque typeconstructor should be a functor:") {
-  REQUIRE(tf::is_functor<std::list>::value == true);
+TEST_CASE("For std::array, calling fmap should induce the same constructor and "
+          "assignment calls as simply using std::transform, plus a move "
+          "construction:",
+          "[CtorAs], [fmap]") {
+  // NB: Extra move construction is because moving a std::array calls move on
+  // each element. So, a move on std::array is O(N) instead of O(1) like the
+  // other containers.
+  std::array<CtorLogger, 2> a;
+  auto b = fmap(id, a);
+
+  std::array<CtorLogger, 2> ra;
+  auto rb = std::array<CtorLogger, 2>{};
+  std::transform(ra.cbegin(), ra.cend(), rb.begin(), id);
+
+  rb[0].flags.push_back(CtorLogger::MoveConstructed);
+  REQUIRE(b[0].flags == rb[0].flags);
+  REQUIRE(b[0].flags == b[1].flags);
 }
 
-TEST_CASE("Given a std::deque<A> …") {
+TEST_CASE("The std::deque typeconstructor should be a functor:") {
+  REQUIRE(tf::is_functor<std::deque>::value == true);
+}
+
+TEST_CASE("Given a std::deque<A> …", "[fmap]") {
   std::deque<A> as{A{}};
 
   SECTION("… and a function f : A → B, fmap should produce a std::deque<B>:") {
@@ -170,8 +222,23 @@ TEST_CASE("Given a std::deque<A> …") {
   }
 }
 
+TEST_CASE("For std::deque, calling fmap should induce the same constructor "
+          "and assignment calls as simply using std::transform.",
+          "[CtorAs], [fmap]") {
+  std::deque<CtorLogger> a(2);
+  auto b = fmap(id, a);
+
+  std::deque<CtorLogger> ra(2);
+  auto rb = std::deque<CtorLogger>{};
+  std::transform(ra.cbegin(), ra.cend(), std::back_inserter(rb), id);
+
+  REQUIRE(b[0].flags == rb[0].flags);
+  REQUIRE(b[0].flags == b[1].flags);
+}
+
 TEST_CASE("Given a function f : A → B, fmap should produce a function object "
-          "which can bind to multiple functor types:") {
+          "which can bind to multiple functor types:",
+          "[fmap], [curried]") {
   auto lifted_f = fmap(f);
   SECTION("… std::vector") {
     std::vector as{A{}};
