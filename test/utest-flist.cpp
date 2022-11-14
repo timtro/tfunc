d39b974d9e75b0d92ef9c1536278f484a65fd368
timtro <timtro@gmail.com>
2022-11-14 15:32:15 -0500

    fmt.

--- test/utest-flist.cpp
+++ test/utest-flist.cpp
@@ -5,21 +5,21 @@
 #include <catch2/catch.hpp>
 
 #include "test-fixtures.hpp"
-using tst::A; // Tag for unit type
-using tst::B; // Tag for unit type
-using tst::C; // Tag for unit type
+using tst::A;  // Tag for unit type
+using tst::B;  // Tag for unit type
+using tst::C;  // Tag for unit type
 using tst::CtorLogger;
-using tst::D;  // Tag for unit type
-using tst::f;  // f : A → B
-using tst::g;  // g : B → C
-using tst::h;  // h : C → D
-using tst::id; // id : T → T
+using tst::D;   // Tag for unit type
+using tst::f;   // f : A → B
+using tst::g;   // g : B → C
+using tst::h;   // h : C → D
+using tst::id;  // id : T → T
 
 #include "../include/function-operations.hpp"
-#include "../include/functor/flist.hpp"
 #include "../include/functor/curried-fmap.hpp"
-using tf::as_functor; // as_functor : G<A> → F<A>;
-using tf::fmap;       // fmap : (A → B) → F<A> → F<B>
+#include "../include/functor/flist.hpp"
+using tf::as_functor;  // as_functor : G<A> → F<A>;
+using tf::fmap;        // fmap : (A → B) → F<A> → F<B>
 
 #include <array>
 #include <deque>
@@ -41,9 +41,10 @@ TEST_CASE("Given a std::vector<A> …", "[fmap]") {
     REQUIRE(bs.at(0) == B{});
   }
 
-  SECTION("… fmap given only a function f : A → B, should produce a function "
-          "object, which when subsequently a std::vector<A> will bind "
-          "appropriately to an uncurried fmap variant.") {
+  SECTION(
+      "… fmap given only a function f : A → B, should produce a function "
+      "object, which when subsequently a std::vector<A> will bind "
+      "appropriately to an uncurried fmap variant.") {
     auto lifted_f = fmap(f);
     REQUIRE((std::is_same<decltype(lifted_f(as))::value_type, B>::value));
     REQUIRE(lifted_f(as).at(0) == B{});
@@ -53,8 +54,9 @@ TEST_CASE("Given a std::vector<A> …", "[fmap]") {
     REQUIRE(fmap(id, as).at(0) == A{});
   }
 
-  SECTION("… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
-          "== fmap(g∘f, a)") {
+  SECTION(
+      "… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
+      "== fmap(g∘f, a)") {
     REQUIRE(fmap(g, fmap(f, as)).at(0) == C{});
     REQUIRE(fmap(g, fmap(f, as)).at(0) == fmap(tf::compose(g, f), as).at(0));
   }
@@ -64,15 +66,16 @@ TEST_CASE("Given a std::vector<A> …", "[fmap]") {
   }
 }
 
-TEST_CASE("For std::vector, calling fmap should induce the same constructor "
-          "and assignment calls as simply using std::transform.",
-          "[CtorAs], [fmap]") {
+TEST_CASE(
+    "For std::vector, calling fmap should induce the same constructor "
+    "and assignment calls as simply using std::transform.",
+    "[CtorAs], [fmap]") {
   std::vector<CtorLogger> a(2);
   auto b = fmap(id, a);
 
   std::vector<CtorLogger> ra(2);
   auto rb = std::vector<CtorLogger>{};
-  rb.reserve(ra.size()); // If no reserve, you get extra copy ctrn!
+  rb.reserve(ra.size());  // If no reserve, you get extra copy ctrn!
   std::transform(ra.cbegin(), ra.cend(), std::back_inserter(rb), id);
 
   REQUIRE(b[0].flags == rb[0].flags);
@@ -92,9 +95,10 @@ TEST_CASE("Given a std::list<A> …", "[fmap]") {
     REQUIRE(*bs.begin() == B{});
   }
 
-  SECTION("… fmap given only a function f : A → B, should produce a function "
-          "object, which when subsequently a std::list<A> will bind "
-          "appropriately to an uncurried fmap variant.") {
+  SECTION(
+      "… fmap given only a function f : A → B, should produce a function "
+      "object, which when subsequently a std::list<A> will bind "
+      "appropriately to an uncurried fmap variant.") {
     auto lifted_f = fmap(f);
     REQUIRE((std::is_same<decltype(lifted_f(as))::value_type, B>::value));
     REQUIRE(*lifted_f(as).begin() == B{});
@@ -104,11 +108,12 @@ TEST_CASE("Given a std::list<A> …", "[fmap]") {
     REQUIRE(*fmap(id, as).begin() == A{});
   }
 
-  SECTION("… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
-          "== fmap(g∘f, a)") {
+  SECTION(
+      "… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
+      "== fmap(g∘f, a)") {
     REQUIRE(*fmap(g, fmap(f, as)).begin() == C{});
-    REQUIRE(*fmap(g, fmap(f, as)).begin() ==
-            *fmap(tf::compose(g, f), as).begin());
+    REQUIRE(*fmap(g, fmap(f, as)).begin()
+            == *fmap(tf::compose(g, f), as).begin());
   }
 
   SECTION("… it should cast to a functor:") {
@@ -116,9 +121,10 @@ TEST_CASE("Given a std::list<A> …", "[fmap]") {
   }
 }
 
-TEST_CASE("For std::list, calling fmap should induce the same constructor and "
-          "assignment calls as simply using std::transform.",
-          "[CtorAs], [fmap]") {
+TEST_CASE(
+    "For std::list, calling fmap should induce the same constructor and "
+    "assignment calls as simply using std::transform.",
+    "[CtorAs], [fmap]") {
   std::list<CtorLogger> a(2);
   auto b = fmap(id, a);
 
@@ -130,9 +136,10 @@ TEST_CASE("For std::list, calling fmap should induce the same constructor and "
   REQUIRE(b.begin()->flags == (b.begin()++)->flags);
 }
 
-TEST_CASE("The std::array typeconstructor will not be a regular functor,  "
-          "because it can not be constructed with the form F<A>. For integer "
-          "dependant type constructors, is_functor_N should test true:") {
+TEST_CASE(
+    "The std::array typeconstructor will not be a regular functor,  "
+    "because it can not be constructed with the form F<A>. For integer "
+    "dependant type constructors, is_functor_N should test true:") {
   REQUIRE((tf::is_functor_N<std::array>::value) == true);
 }
 
@@ -145,9 +152,10 @@ TEST_CASE("Given a std::array<A,·> …", "[fmap]") {
     REQUIRE(bs.at(0) == B{});
   }
 
-  SECTION("… fmap given only a function f : A → B, should produce a function "
-          "object, which when subsequently a std::array<A> will bind "
-          "appropriately to an uncurried fmap variant.") {
+  SECTION(
+      "… fmap given only a function f : A → B, should produce a function "
+      "object, which when subsequently a std::array<A> will bind "
+      "appropriately to an uncurried fmap variant.") {
     auto lifted_f = fmap(f);
     REQUIRE((std::is_same<decltype(lifted_f(as))::value_type, B>::value));
     REQUIRE(lifted_f(as).at(0) == B{});
@@ -156,8 +164,9 @@ TEST_CASE("Given a std::array<A,·> …", "[fmap]") {
   SECTION("… mapping over it with the id-function should leave it unchanged") {
     REQUIRE(fmap(id, as).at(0) == A{});
   }
-  SECTION("… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
-          "== fmap(g∘f, a)") {
+  SECTION(
+      "… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
+      "== fmap(g∘f, a)") {
     REQUIRE(fmap(g, fmap(f, as)).at(0) == C{});
     REQUIRE(fmap(g, fmap(f, as)).at(0) == fmap(tf::compose(g, f), as).at(0));
   }
@@ -167,10 +176,11 @@ TEST_CASE("Given a std::array<A,·> …", "[fmap]") {
   }
 }
 
-TEST_CASE("For std::array, calling fmap should induce the same constructor and "
-          "assignment calls as simply using std::transform, plus a move "
-          "construction:",
-          "[CtorAs], [fmap]") {
+TEST_CASE(
+    "For std::array, calling fmap should induce the same constructor and "
+    "assignment calls as simply using std::transform, plus a move "
+    "construction:",
+    "[CtorAs], [fmap]") {
   // NB: Extra move construction is because moving a std::array calls move on
   // each element. So, a move on std::array is O(N) instead of O(1) like the
   // other containers.
@@ -199,9 +209,10 @@ TEST_CASE("Given a std::deque<A> …", "[fmap]") {
     REQUIRE(bs.at(0) == B{});
   }
 
-  SECTION("… fmap given only a function f : A → B, should produce a function "
-          "object, which when subsequently a std::deque<A> will bind "
-          "appropriately to an uncurried fmap variant.") {
+  SECTION(
+      "… fmap given only a function f : A → B, should produce a function "
+      "object, which when subsequently a std::deque<A> will bind "
+      "appropriately to an uncurried fmap variant.") {
     auto lifted_f = fmap(f);
     REQUIRE((std::is_same<decltype(lifted_f(as))::value_type, B>::value));
     REQUIRE(lifted_f(as).at(0) == B{});
@@ -211,8 +222,9 @@ TEST_CASE("Given a std::deque<A> …", "[fmap]") {
     REQUIRE(fmap(id, as).at(0) == A{});
   }
 
-  SECTION("… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
-          "== fmap(g∘f, a)") {
+  SECTION(
+      "… it should satisfy the composition relation: fmap(g, fmap(f, a)) "
+      "== fmap(g∘f, a)") {
     REQUIRE(fmap(g, fmap(f, as)).at(0) == C{});
     REQUIRE(fmap(g, fmap(f, as)).at(0) == fmap(tf::compose(g, f), as).at(0));
   }
@@ -222,9 +234,10 @@ TEST_CASE("Given a std::deque<A> …", "[fmap]") {
   }
 }
 
-TEST_CASE("For std::deque, calling fmap should induce the same constructor "
-          "and assignment calls as simply using std::transform.",
-          "[CtorAs], [fmap]") {
+TEST_CASE(
+    "For std::deque, calling fmap should induce the same constructor "
+    "and assignment calls as simply using std::transform.",
+    "[CtorAs], [fmap]") {
   std::deque<CtorLogger> a(2);
   auto b = fmap(id, a);
 
@@ -236,9 +249,10 @@ TEST_CASE("For std::deque, calling fmap should induce the same constructor "
   REQUIRE(b[0].flags == b[1].flags);
 }
 
-TEST_CASE("Given a function f : A → B, fmap should produce a function object "
-          "which can bind to multiple functor types:",
-          "[fmap], [curried]") {
+TEST_CASE(
+    "Given a function f : A → B, fmap should produce a function object "
+    "which can bind to multiple functor types:",
+    "[fmap], [curried]") {
   auto lifted_f = fmap(f);
   SECTION("… std::vector") {
     std::vector as{A{}};
